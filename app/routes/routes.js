0b83d665a1e0bb8a2ae38e4b65910f2d4ac9a70f
Dylan Devalia <dylan@devalia.com>
2018-08-14 17:39:26 +0100

    Messed around with partials

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -6,18 +6,23 @@ var express = require('express');
 var router = express.Router();
 
 // home page ( index for games and new player )
-router.get(['/','/home','main'],(req, res, next) => {
-  res.render('home',{title:'Home'})
+router.get(['/','/home','/main'],(req, res, next) => {
+  res.render('home',{title:'home'})
+});
+
+// home page ( index for games and new player )
+router.get(['/asdf'],(req, res, next) => {
+  res.render('home',{title:'asdf'})
 });
 
 // player output
 router.get('/player',(req, res, next)=>{
-  res.render('home',{title:'Home'})
+  res.render('player',{title:'player'})
 })
 
 // render output
 router.get('/screen',(req, res, next)=>{
-  res.render('home',{title:'Home'})
+  res.render('screen',{title:'Home'})
 })
 
 // exporting module
