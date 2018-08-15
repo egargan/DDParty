0598682ec19e50c2defe4e3f12f60f91bc5e2f9f
Dylan Devalia <dylan@devalia.com>
2018-08-15 14:29:26 +0100

    Created simple player controller

---
+++ app/game/bundle_player.js
@@ -0,0 +1,21 @@
+
+// css
+require('../../scss/player.scss')
+
+// main file for launching game code
+// require('./client/Balls.js');
+// require('./client/app.js');
+
+// importing shared files
+// require('./shared/message.js')
+//
+// // grageo files
+//
+// // requiring main module
+// require('./grageo/grageo.js');
+//
+// // importing lib files
+// require('./grageo/colour.js');
+// require('./grageo/vector.js');
+// require('./grageo/canvaslayer.js');
+// require('./grageo/basic.js');
